d6f7203a932fb5c9a5dc8bfdd407ffac8b5eb4cf
Aria Williams <awill9579@gmail.com>
2026-09-07 22:38:16 -0500

    Main Menu Setup

---
+++ Framework/Content/MainMenu/main_menu.gd
@@ -0,0 +1,21 @@
+class_name MainMenu extends Control
+
+@onready var start: Button = %Start
+@onready var settings: Button = %Settings
+@onready var exit: Button = %Exit
+
+# all buttons on left side
+# on load: slide in from left
+# on select: larger size, shift others up/down depending
+# on click: flash color as other options slide away, then slide away
+
+func _on_start_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
